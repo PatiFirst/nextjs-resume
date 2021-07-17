712c4a4fafc4ecdd6b3066f60eb5b01365a197c1
Patipon W <patipon888@gmail.com>
2021-07-17 18:29:11 +0700

    Update images

---
+++ pages/data/allProject.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        id: 1,
+        category: 'project',
+        title: 'ShirBand Shop',
+        subtitle: 'A clothing shopping online for women on web application',
+        description: 'This is a project in the course "Web Programming" of Media Technology and I am full stack developer to make a shopping page and admin page. In front-end, I have made that with HTML, CSS, JavaScript and Bootstrap. And Back-end, I use Firebase for Database and Hosting this web app.'
+    },
+    {
+        id: 2,
+        category: 'project',
+        title: 'Muscular (Lower)',
+        subtitle: 'An AR application about "Muscular System (Lower Boudy)"',
+        description: 'This is a mini-project in the course of Media Technology. This is an AR application for learning an anatomy about "Lower Muscular System" with Marker-based AR. This app is made with Unity to make a application and Vuforia to build and run AR in this application'
+    },
+    {
+        id: 3,
+        category: 'project',
+        title: 'HeartCir',
+        subtitle: 'A 3D Instruction programme about Blood Circulation System with heart rate module',
+        description: 'This is a mini-project in the course of Media Technology. HeartCir is made with Unity and the heart rate module is made with Arduino. HeartCir is simulate a heartbeat of human heart. This programme has 2 system. First system, you can set rate to show how heart rate. Another system is a real time, you can use the heart rate module to show how your heart beats'
+    },
+    {
+        id: 4,
+        category: 'project',
+        title: 'Rescape',
+        subtitle: 'A VR game with heart rate module to escape the robots',
+        description: `This is a project in "NSC2021". Rescape is a VR-RPG game that made with Unity and play with heart rate module that made with Arduino. I'm responsible to make a heart rate module and design some map in this game
+
+        The heart rate module during in this game is show on right hand and depend on a Detect Circle. If detect circle is detect to a robot, the game will restart. So If detect circle is wider, player will escape harder.
+        
+        Rescape is a game about "Teen" who is growing up with "MOM" that is a robot. One Day, he find "Will" who have injury and talking with him how to escape this place with the robots.`
+    },
+    {
+        id: 5,
+        category: 'project',
+        title: 'SweetBoy Runner',
+        subtitle: 'A 3D endless game',
+        description: `This is a mini-project in the course "Computer Programming" of Media Technology. SweetBoy Runner is a 3D endless game that made with Unity.
+
+        This game is about a boy who runs to collect as many snacks as possible. When running into an object, the game will be over.`
+    }
+    
+]
